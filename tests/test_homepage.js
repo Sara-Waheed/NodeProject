df293507745fe1df7d5f601d679c84a49df12b73
Sara-Waheed <143251188+Sara-Waheed@users.noreply.github.com>
2025-05-25 15:46:46 +0500

    Update test_homepage.js

---
+++ tests/test_homepage.js
@@ -0,0 +1,30 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const path = require('path');
+const os = require('os');
+
+(async function homepage() {
+  // create unique temp dir for Chrome profile
+  const userDataDir = path.join(os.tmpdir(), `wd-profile-${Date.now()}`);
+
+  let options = new chrome.Options()
+    .addArguments(
+      '--headless=new',
+      '--no-sandbox',
+      '--disable-dev-shm-usage',
+      `--user-data-dir=${userDataDir}`
+    );
+
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+
+  try {
+    await driver.get('http://host.docker.internal:3000/');
+    await driver.wait(until.titleContains('Dummy Node App'), 5000);
+    console.log('Homepage test passed');
+  } finally {
+    await driver.quit();
+  }
+})();
